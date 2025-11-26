7d4be9455dba228ec613cfbbe04d2254caffa27c
brunocoelhoribas <bruno200coelho@hotmail.com>
2025-11-26 11:20:51 -0300

    Fix: unit tests

--- tests/Feature/OpenWeatherServiceTest.php
+++ tests/Feature/OpenWeatherServiceTest.php
@@ -9,45 +9,51 @@ use Illuminate\Support\Facades\Cache;
 use Exception;
 
 class OpenWeatherServiceTest extends TestCase {
-    public function testItFetchesWeatherDataAndReturnsValidDto(): void {
-        Http::fake([
-            'api.openweathermap.org/*' => Http::response([
+    public function testFetchesCurrentWeather(): void {
+        Http::fake(static function ($request) {
+            $url = $request->url();
+
+            if (str_contains($url, 'air_pollution')) {
+                return Http::response([
+                    'list' => [
+                        ['main' => ['aqi' => 3]]
+                    ]
+                ]);
+            }
+
+            return Http::response([
                 'name' => 'Navegantes',
                 'main' => [
-                    'temp' => 25.5,
-                    'feels_like' => 27.0,
-                    'temp_min' => 24.0,
-                    'temp_max' => 26.0,
-                    'humidity' => 70,
+                    'temp' => 20.0,
+                    'feels_like' => 19.5,
+                    'temp_min' => 18.0,
+                    'temp_max' => 22.0,
+                    'humidity' => 80,
                     'pressure' => 1012
                 ],
                 'weather' => [
-                    ['description' => 'scattered clouds', 'icon' => '03d']
+                    ['description' => 'light rain', 'icon' => '10d']
                 ],
-                'wind' => ['speed' => 5.5],
+                'wind' => ['speed' => 5.0],
                 'coord' => ['lat' => -26.89, 'lon' => -48.65],
                 'visibility' => 10000,
                 'dt' => 1630000000,
                 'timezone' => -10800,
-                'sys' => ['sunrise' => 1630000000, 'sunset' => 1630040000]
-            ]),
-
-            '*/air_pollution*' => Http::response([
-                'list' => [
-                    ['main' => ['aqi' => 3]]
+                'sys' => [
+                    'sunrise' => 1630000000,
+                    'sunset' => 1630040000
                 ]
-            ])
-        ]);
+            ]);
+        });
 
         Cache::flush();
 
         $service = new OpenWeatherService();
-        $result = $service->getCurrentWeather('Navegantes');
+        $dto = $service->getCurrentWeather('Navegantes');
 
-        $this->assertEquals('Navegantes', $result->city);
-        $this->assertEquals(25.5, $result->temperature);
-        $this->assertEquals(3, $result->aqi);
-        $this->assertNotEmpty($result->advice);
+        $this->assertEquals('Navegantes', $dto->city);
+        $this->assertEquals(3, $dto->aqi);
+        $this->assertStringContainsString('umbrella', $dto->advice);
     }
 
     public function testThrowsExceptionIfCityNotFound(): void {
